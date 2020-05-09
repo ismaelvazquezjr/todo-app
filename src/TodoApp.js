215cc404ead548f9a77b331e2b36624b36c84e04
Ismael Vazquez Jr <vazquezjrismael@gmail.com>
2020-05-08 18:34:38 -0700

    Rewriting things to work with backend

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -22,7 +22,7 @@ class TodoApp extends Component {
     }
 
     componentDidMount() {
-        fetch("/get")
+        fetch("https://thegibson.directory:3000/get")
         .then(res => res.json())
         .then(data => {
             if (data === null) return;
@@ -34,45 +34,67 @@ class TodoApp extends Component {
     }
 
     getTasks() {
-        fetch("/get")
+        fetch("https://thegibson.directory:3000/get")
         .then(res => res.json())
         .then(data => {
-            if (data === null) return;
+            if (data === null) data = [];
             this.setState({todos: data});
         });
     }
 
     changeHandler(evt) {
         this.setState({
-            [evt.target.name]: [evt.target.value]
+            [evt.target.name]: evt.target.value
         });
     }
 
     updateStatus(idx, status) {
-        const endpoint = "/update?id=" + this.state.todos[idx].id;
+        const endpoint = "https://thegibson.directory:3000/update?id=" + this.state.todos[idx].id;
         fetch(endpoint)
         .then(() => {
             this.getTasks();
-        })
+        });
     }
 
     removeTodo(idx) {
-        let todos = [...this.state.todos];
-        todos.splice(idx, 1);
-        this.setState({ todos: todos});
+        const endpoint = "https://thegibson.directory:3000/delete?id=" + this.state.todos[idx].id;
+        fetch(endpoint)
+        .then(() => {
+            this.getTasks();
+        });
     }
 
     addItem() {
         const newTodoObj = {
             task: this.state.task,
-            id: uuid()
+            id: uuid(),
+            created: new Date().getTime().toString(),
+            completed: 0
         };
-        this.setState(currState => {
-            return {
-                todos: [...currState.todos, newTodoObj],
-                task: ""
-            }
+        this.postData(newTodoObj)
+        .then(() => {
+            this.setState({task: ""});
+            this.getTasks();
         });
+    }
+
+    // Example POST method implementation:
+    async postData(data) {
+        // Default options are marked with *
+        const response = await fetch("https://thegibson.directory:3000/add", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response; // parses JSON response into native JavaScript objects
     }
 
     render() {
